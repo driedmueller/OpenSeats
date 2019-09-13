01899f6553a4eb6bd32b1e7b95831550e9d2bfb2
Dan R <driedmueller@hotmail.com>
2019-09-13 16:16:26 -0500

    readMail is now working and add term, crn, email to .csv file

--- OpenSeats/readMail.py
+++ OpenSeats/readMail.py
@@ -1,14 +1,35 @@
-import imaplib
-import email
-import keyring
+import imaplib, email, keyring, re, csv
 
 email_user = 'openseat1909@gmail.com'
 email_password = keyring.get_password('yagmail', 'openseat1909@gmail.com')
 
-mail = imaplib.IMAP4_SSL('imap.gmail.com') #port 993
-mail.login(email_user, email_password)
-mail.select('Inbox')
+try:
+    mail = imaplib.IMAP4_SSL('imap.gmail.com') #port 993
+    mail.login(email_user, email_password)
+    mail.select('Inbox')
 
-type, data = mail.search(None, '(SUBJECT "test")')
-mail_ids = data[0]
-id_list = mail_ids.split()
+    type, data = mail.search(None, 'ALL')
+    mail_ids = data[0]
+    id_list = mail_ids.split()
+    latest_email = ''
+
+    for num in reversed(id_list):
+        typ, data = mail.fetch(num, '(RFC822)')
+
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_string(response_part[1].decode('utf-8'))
+                email_subject = msg['subject']
+                email_from = msg['from']
+                if (email_subject.startswith('202020')) or (email_subject.startswith('201980')):
+                    sender = re.findall(r'<(.*?)>', email_from)[0] #grabs e-mail address between < and >
+                    term, crn = email_subject.split(' ', 1) #subject line splits at the space
+                    print(sender)
+                    print(email_subject)
+                    #add e-mail, term, and crn to csv file
+                    with open('input.csv', 'a', newline='') as file:
+                        write = csv.writer(file)
+                        write.writerow([term, crn, sender])
+                            
+except Exception as e:
+    print(str(e))
